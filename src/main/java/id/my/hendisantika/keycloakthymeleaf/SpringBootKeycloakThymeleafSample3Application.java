60dd68663ebdb44d4a19d7cf058fc4167894d106
hendisantika <hendisantika@yahoo.co.id>
2024-10-13 07:08:54 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/keycloakthymeleaf/SpringBootKeycloakThymeleafSample3Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.keycloakthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloakThymeleafSample3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKeycloakThymeleafSample3Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/keycloakthymeleaf/SpringBootKeycloakThymeleafSample3ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.keycloakthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakThymeleafSample3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
